205f5060429319e0e909f4903454245afea8c846
mmilind <milind.mahadik@gmail.com>
2019-01-14 15:13:56 +1100

    Template python file demonstrating import of all important python libraries for data science.

---
+++ py-datasci/python_datasci_ecosystem.py
@@ -0,0 +1,22 @@
+#!/Users/milind/anaconda3/bin/python
+
+
+# program to demonstrate various ways of using pandas
+
+import pandas as pd
+import numpy as np
+import scipy as sp
+import sklearn as skl
+import patsy
+import matplotlib as mtplt
+import statsmodels as sm
+
+
+print(pd.__version__)
+print(np.__version__)
+print(sp.__version__)
+print(skl.__version__)
+print(mtplt.__version__)
+print(sm.__version__)
+
+
